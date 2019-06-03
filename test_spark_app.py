57a006ca4d309b863ae4e96ed3bdc824b4aa45b0
fabian-fuentealba <f.fuentealba.acuna@gmail.com>
2019-06-03 00:18:42 -0400

    Se agrega modulo de test unitarios para creacion de sparkContext y extraccion de archivo web

--- test_spark_app.py
+++ test_spark_app.py
@@ -2,9 +2,20 @@
 
 import unittest
 import spark_app
+from pyspark.sql import SparkSession
 
 class TestMyModule(unittest.TestCase):
 
-    def test_extract( self ):
+    def test_spark_obj( self ):
+        self.assertEqual(spark_app.spark_obj('TEST')['type'], True )
 
-        
+    def test_extract( self ):
+        url = "https://s3.amazonaws.com/rrhh-test/clean_test.csv"
+        spark = spark_app.spark_obj("TEST_DE")['obj']
+        self.assertEqual(spark_app.extract( url, spark )['type'], True )       
+
+
+
+if __name__ == "__main__":
+
+    unittest.main()
