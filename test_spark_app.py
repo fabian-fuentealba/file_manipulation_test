102acdeb21384c8e2c95e64cace7fe7e7c109930
fabian-fuentealba <f.fuentealba.acuna@gmail.com>
2019-06-03 00:33:25 -0400

    Se agrega test sin envio de url archivo

--- test_spark_app.py
+++ test_spark_app.py
@@ -12,7 +12,12 @@ class TestMyModule(unittest.TestCase):
     def test_extract( self ):
         url = "https://s3.amazonaws.com/rrhh-test/clean_test.csv"
         spark = spark_app.spark_obj("TEST_DE")['obj']
-        self.assertEqual(spark_app.extract( url, spark )['type'], True )       
+        self.assertEqual(spark_app.extract( url, spark )['type'], True )
+
+    def test_extract_error( self ):
+        url = ""
+        spark = spark_app.spark_obj("TEST_DE")['obj']
+        self.assertEqual(spark_app.extract( url, spark )['type'], False )       
 
 
 
